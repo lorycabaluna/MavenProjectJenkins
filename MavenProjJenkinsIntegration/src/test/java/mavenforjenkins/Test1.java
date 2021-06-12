1fddec8ec87d504adbeb4417dd5e6f4d38ed71a6
MacBook Pro 15' 2014 Retina <macbookpro152014retina@MacBooks-MacBook-Pro.local>
2021-06-12 17:44:35 +0800

    first commit

---
+++ MavenProjJenkinsIntegration/src/test/java/mavenforjenkins/Test1.java
@@ -0,0 +1,29 @@
+package mavenforjenkins;
+
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+	
+	
+	
+	@Test
+	public void startBrowser() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"),"Title does not match");
+		driver.quit();
+		
+	}
+	
+
+}
